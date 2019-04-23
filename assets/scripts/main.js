9b76c9d80f3046f315cd2fe6221d3ca81e9e9f49
DamColson <damien.colson@gmail.com>
2019-04-23 21:53:35 +0200

    probleme réglé sur la class absolute, images mise en place, certain media mis en place, reste des panneaux a remplir

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,42 +1,47 @@
-$(document).ready(function(){
+
 
     $("#toSlideOne").click(function(){
+      var productPosition = $('#product').position();
         $('#thirdSlide').hide('slide', {direction: 'right'}, 500);
         $('#secondSlide').hide('slide', {direction: 'right'}, 500);
         $('#listing').hide(500);
-        $('#product').addClass('absolute');
+        $('#product').css('position','absolute').css('top',productPosition.top + 'px');
         $('#firstSlide').delay(550).show('slide', {direction: 'left'}, 500);
         $('#listing').delay(200).show(500);
-        $('#product').delay(10000).removeClass('absolute');
+
     });
     $('#toSlideTwo').click(function(){
+      var productPosition = $('#product').position();
       if($('#thirdSlide').css('display') !== 'none' && $('#thirdSlide').css('visibility') !== 'hidden' && $('#thirdSlide').css('opacity') !== 0){
       $('#firstSlide').hide('slide', {direction: 'left'}, 500);
       $('#thirdSlide').hide('slide', {direction: 'right'}, 500);
       $('#listing').hide(500);
-      $('#product').addClass('absolute');
+      $('#product').css('position','absolute').css('top',productPosition.top + 'px');
       $('#secondSlide').delay(550).show('slide', {direction: 'left'}, 500)
       $('#listing').delay(200).show(500);
-      $('#product').delay(10000).removeClass('absolute');
     }else{
       $('#firstSlide').hide('slide', {direction: 'left'}, 500);
       $('#thirdSlide').hide('slide', {direction: 'right'}, 500);
       $('#listing').hide(500);
-      $('#product').addClass('absolute');
+      $('#product').css('position','absolute').css('top',productPosition.top + 'px');
       $('#secondSlide').delay(550).show('slide', {direction: 'right'}, 500)
       $('#listing').delay(200).show(500);
-      $('#product').delay(10000).removeClass('absolute');
     }
     });
     $('#toSlideThree').click(function(){
+      var productPosition = $('#product').position();
       $('#firstSlide').hide('slide', {direction: 'left'}, 500);
       $('#secondSlide').hide('slide', {direction: 'left'}, 500);
       $('#listing').hide(500);
-      $('#product').addClass('absolute');
+      $('#product').css('position','absolute').css('top',productPosition.top + 'px');
       $('#thirdSlide').delay(550).show('slide', {direction: 'right'}, 500);
       $('#listing').delay(200).show(500);
-      $('#product').delay(10000).removeClass('absolute');
-
     });
 
-});
+    $('#product').mouseover(function(){
+      $('#product').css('position','');
+    });
+
+    $(window).resize(function(){
+      $('#product').css('position','');
+    });
