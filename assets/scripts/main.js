6758b6027e711c6bbd01c48ca5a651f55424bb03
DamColson <damien.colson@gmail.com>
2019-04-25 10:01:43 +0200

    dernier reglage a faire

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -54,5 +54,51 @@
     $(window).resize(function(){
       $('#product').css('position','');
     });
+    //au clic de n'importe quelle image se trouvant dans la div product, lancera la fonction suivante
+    $('#product img').click(function(){
+      //vide l'intégralité du text se trouvant dans le body de la modal popup
+      $('#modalBody').text('');
+      //déclaration de diverse variable récupérant la source de l'image, le contenu de l'attribut data-item, le prix stocké dans la base de données ainsi que la description et le titre stockée dans la abse de donnée
+      var src = $(this).attr('src');
+      var data = $(this).attr('data-item');
+      var price = $('#' + data + ' .price').text();
+      //split de la class description afin de séparé dans un tableau le titre de la description de l'objet
+      var descriptionTable = $('#' + data + ' .description').text().split('/');
+      //variable contenant la balise img complete qui sera incorporée a la modal via le js
+      var img = '<img class="img-fluid mb-2 mb-md-0 rounded" src=\'' + src + '\' alt=\'' + descriptionTable[0] + '\' title=\'' + descriptionTable[0] + '\' id="imgModal" />'
+      //Ajout du premier element du tableua créer précédement dans le titre de la modal
+      $('#productModalTitle').text(descriptionTable[0]);
+      //ajout de l'image dans le body de la modal
+      $('#modalBody').append(img);
+      //resize de l'image pour que sa taille soit adaptée a la modal
+      $('#imgModal').css('width','25%');
+      //Ajout de la descrition a la suite de l'image dans le body de la modal
+      $('#modalBody').append('<p>' + descriptionTable[1] + '</p>');
+      //ajout du prix a la suite de la description dans le body de la modal
+      $('#modalBody').append(price + '€');
+    });
 
-    
+    //idem que pour la précédente mais lorsque l'on click sur les image de la div tendancies
+    $('#tendancies img').click(function(){
+      $('#modalBody').text('');
+      var src = $(this).attr('src');
+      var data = $(this).attr('data-item');
+      var price = $('#' + data + ' .price').text();
+      var descriptionTable = $('#' + data + ' .description').text().split('/');
+      var img = '<img class="img-fluid mb-2 mb-md-0 rounded" src=\'' + src + '\' alt=\'' + descriptionTable[0] + '\' title=\'' + descriptionTable[0] + '\' id="imgModal" />'
+      $('#productModalTitle').text(descriptionTable[0]);
+      $('#modalBody').append(img);
+      $('#imgModal').css('width','25%');
+      $('#modalBody').append('<p>' + descriptionTable[1] + '</p>');
+      $('#modalBody').append(price + '€');
+    });
+
+    //au click d'une image se trouvant dans la div product, ajout d'un attribut data item au bouton de la modal contenant les donnée du data item de l'image sur laquelle on a cliqué
+    $('#product img').click(function(){
+      $('#addToCart').attr('data-item', $(this).attr('data-item'));
+    });
+
+    //meme chose que précédement mais pour les images se trouvant dans la div tendancies
+    $('#tendancies img').click(function(){
+      $('#addToCart').attr('data-item', $(this).attr('data-item'));
+    });
